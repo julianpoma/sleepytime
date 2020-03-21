10db23a0e21bbeaa8857e7f291cba14e46ea1082
Julian Poma <poma.nob@gmail.com>
2020-03-21 20:48:47 -0300

    Initial setup configuration

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: ['@react-native-community'],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    'sort-imports': [
+      1,
+      {
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'no-shadow': 1,
+    'no-param-reassign': 1,
+    'no-console': 1,
+    'implicit-arrow-linebreak': 0,
+    'prefer-arrow-callback': 2,
+    'react/prop-types': 0,
+    'react/destructuring-assignment': ['warn'],
+    'react-native/no-color-literals': 0,
+    'react/prefer-stateless-function': 1,
+    'sort-keys': [
+      'error',
+      'asc',
+      { caseSensitive: true, natural: false, minKeys: 2 },
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'jsx-quotes': ['error', 'prefer-double'],
+  },
+};
---
+++ app/App.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const App: React.FC<{}> = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default App;
